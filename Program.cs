c2c4aa4be796f6e0da88b28a3e04ace87bac9d85
pashwiniis <pashwini684@gmail.com>
2021-12-22 11:02:28 +0530

    Start the game and stake is 00

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblerProblem
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Gambler gambler = new Gambler();
+            gambler.Start();
+        }
+
+    }
+}
